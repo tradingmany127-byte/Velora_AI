ce4040f5cd3bc5aea7c23ba8ba677c8d3ae266e6
Irakko <tradingmany127@gmail.com>
2026-03-03 20:53:20 +0100

    fix

--- public/app.js
+++ public/app.js
@@ -289,7 +289,7 @@ function showWelcome(name) {
   elWelcome.onclick = (e) => { if (e.target === elWelcome) { elWelcome.classList.add("hidden"); elWelcome.innerHTML=""; } };
 }
 
-function openAuthModal() {
+function openAuthModal() {}
   const body = `
     <div class="sub">
       Регистрация: <b>Имя</b> + <b>Email</b> + <b>Пароль</b> → код из письма (6 цифр).<br/>
@@ -337,7 +337,7 @@ function openAuthModal() {
   </button>
 </div>
 </div>
-}
+
     `;
 
 
